180badf174dbcdee4f54c3d86a22dc5f4a85c30b
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-02-26 13:00:43 +0300

    Create_Grid_Controller

--- Assets/Scripts/GamePlay/Grid/GridController.cs
+++ Assets/Scripts/GamePlay/Grid/GridController.cs
@@ -1,21 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Random = UnityEngine.Random;
 
 namespace GamePlay.Grid
 {
     public class GridController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private string[] tileAddressableKeys;
+        [SerializeField] private int gridWidth = 8;
+        [SerializeField] private int gridHeight  = 8;
+        [SerializeField] private float tileSpacing = 1.1f;
+
+        private void Start()
         {
-        
+            GenerateGrid();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void GenerateGrid()
         {
-        
+            float offsetX = ((gridWidth - 1) * tileSpacing) / 2f;
+            float offsetY = ((gridHeight - 1) * tileSpacing) / 2f;
+            
+            for (int x = 0; x < gridWidth; x++)
+            {
+                for (int y = 0; y < gridHeight; y++)
+                {
+                    Vector3 pos = new Vector3(x * tileSpacing - offsetX, y * tileSpacing - offsetY, 0f);
+                    int randomIndex = Random.Range(0, tileAddressableKeys.Length);
+                    string key = tileAddressableKeys[randomIndex];
+
+                    Addressables.InstantiateAsync(key, pos, Quaternion.identity, transform);
+                }
+            }
         }
     }
 }
